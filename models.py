eac2252a641d7ca65c7fc0dba9284d1e1f75b67b
Shresth Verma <vermashresth@gmail.com>
2020-05-30 19:36:30 +0530

    models

--- models.py
+++ models.py
@@ -5,7 +5,46 @@ from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 
 tf = try_import_tf()
 
-class CustomModel1(TFModelV2):
+class SpeakerModel(TFModelV2):
+    """Custom model for policy gradient algorithms."""
+
+    def __init__(self, obs_space, action_space, num_outputs, model_config,
+                 name):
+        super(CustomModel1, self).__init__(obs_space, action_space,
+                                           num_outputs, model_config, name)
+        self.inputs = tf.keras.layers.Input(
+            shape=obs_space.shape, name="observations")
+        layer_1 = tf.keras.layers.Dense(
+            4,
+            name="my_layer1",
+            activation=tf.nn.relu,
+            kernel_initializer=normc_initializer(1.0))(self.inputs)
+        layer_2 = tf.keras.layers.Dense(
+            4,
+            name="my_layer2",
+            activation=tf.nn.relu,
+            kernel_initializer=normc_initializer(1.0))(layer_1)
+        layer_out = tf.keras.layers.Dense(
+            num_outputs,
+            name="my_out",
+            activation=None,
+            kernel_initializer=normc_initializer(0.01))(layer_2)
+        value_out = tf.keras.layers.Dense(
+            1,
+            name="value_out",
+            activation=None,
+            kernel_initializer=normc_initializer(0.01))(layer_1)
+        self.base_model = tf.keras.Model(self.inputs, [layer_out, value_out])
+        self.register_variables(self.base_model.variables)
+
+    def forward(self, input_dict, state, seq_lens):
+        model_out, self._value_out = self.base_model(input_dict["obs"])
+        return model_out, state
+
+    def value_function(self):
+        return tf.reshape(self._value_out, [-1])
+
+class ListenerModel(TFModelV2):
     """Custom model for policy gradient algorithms."""
 
     def __init__(self, obs_space, action_space, num_outputs, model_config,
